c14fa617b6be4207f00850949501c45ff3427454
duclunlun <duclunlun@gmail.com>
2024-04-08 16:49:45 -0400

    init 2

--- trip_planner_agent/utilities.py
+++ trip_planner_agent/utilities.py
@@ -1,15 +1,8 @@
-# from langchain_core.messages import AIMessage, HumanMessage
-# from langchain_community.document_loaders import WebBaseLoader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_community.vectorstores import Chroma
-# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-# from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-# from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
-
+# get the LLM
 def getLLM(model):
     load_dotenv()   
     if model == "GOOGLE_API_KEY":
